eecc64a0bb9b5a9e5ed993a39e36a31299fb75eb
Mike W <mwwpublic@gmail.com>
2017-05-29 11:21:23 -0400

    add click checking

--- Assets/Map/Province.cs
+++ Assets/Map/Province.cs
@@ -1,15 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Map : MonoBehaviour {
+public class Province : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void OnSelect () {
+        Debug.Log("test");
+    }
 }
---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour {
+    private Dictionary<string, UnityEvent> events;
+
+    private static EventManager eventManager;
+
+    public static EventManager Instance {
+        get {
+            if (!eventManager) {
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+
+                if (!eventManager) {
+                    Debug.LogError("There needs to be one active EventManager script");
+                } else {
+                    eventManager.Init();
+                }
+            }
+
+            return eventManager;
+        }
+    }
+
+    public static void AddListener(string name, UnityAction listener) {
+        UnityEvent addedEvent = null;
+        if (Instance.events.TryGetValue(name, out addedEvent)) {
+            addedEvent.AddListener(listener);
+        } else {
+            addedEvent = new UnityEvent();
+            addedEvent.AddListener(listener);
+            Instance.events.Add(name, addedEvent);
+        }
+    }
+
+    void Init() {
+        if (events == null) {
+            events = new Dictionary<string, UnityEvent>();
+        }
+    }
+}
